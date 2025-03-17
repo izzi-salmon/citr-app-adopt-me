b29297440ed83d293cf563d8041de5332e4af852
Izzi Salmon <izzi.salmon@gmail.com>
2025-03-17 16:47:39 +1300

    added package manager and linter

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+// Capitalising "App" is required
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Spaghetti",
+      animal: "Dog",
+      breed: "Dachshund",
+    }),
+    React.createElement(Pet, {
+      name: "Chomp",
+      animal: "Dog",
+      breed: "German Shepard",
+    }),
+    React.createElement(Pet, {
+      name: "Splat",
+      animal: "Cat",
+      breed: "Domestic short hair",
+    })
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
